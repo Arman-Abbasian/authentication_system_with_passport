17232a5b84f5e0ee3927a0979ca338e7408f79f9
Arman Abbasian <abasian.arman@gmail.com>
2023-05-02 14:18:45 +0430

    make to middleware for check the authenticated user

--- middlewares/passportAuthentication.js
+++ middlewares/passportAuthentication.js
@@ -7,4 +7,18 @@ return passport.authenticate(strategyName,{
     failureFlash:flashCondition
 })
 };
-module.exports={passportAuthentication}
+//this middleware use in the pages that authenticated user is need
+function checkAuthentication(req,res,next){
+    if(req.isAuthenticated()) return next();
+    return res.redirect("/login")
+}
+//this route is used when autheticated user want to go to register or login page 
+function redirectIfIsAuth(req,res,next){
+    if(req.isAuthenticated()) res.redirect ("/profile")
+    return next()
+}
+module.exports={
+    passportAuthentication,
+    checkAuthentication,
+    redirectIfIsAuth
+}
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,14 @@
 const {Router}=require("express");
 const { indexPage, loginPage, profilePage, registerPage, registerNewUser, loginUser } = require("../controllers/index.controllers");
-const { passportAuthentication } = require("../middlewares/passportAuthentication");
+const { passportAuthentication, redirectIfIsAuth, checkAuthentication } = require("../middlewares/passportAuthentication");
 const router=Router();
 function initRoutes(passport){
     router.get("/",indexPage)
-router.get("/login",loginPage)
-router.get("/profile",profilePage)
-router.get("/register",registerPage)
-router.post("/register",registerNewUser);
-router.post("/login",passportAuthentication('local','profile','login',true),loginUser);
+router.get("/login",redirectIfIsAuth,loginPage)
+router.get("/profile",checkAuthentication,profilePage)
+router.get("/register",redirectIfIsAuth,registerPage)
+router.post("/register",redirectIfIsAuth,registerNewUser);
+router.post("/login",redirectIfIsAuth,passportAuthentication('local','profile','login',true),loginUser);
 return router;
 }
 module.exports={
